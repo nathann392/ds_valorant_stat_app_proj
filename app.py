3e4438ef2e9377147bfafeea414228a824e5f150
Nathan Nguyen <nathann392@gmail.com>
2022-11-28 03:45:20 -0700

    Load player and refactor

--- app.py
+++ app.py
@@ -22,10 +22,7 @@ import sys
 
 
 # Web scraping
-@st.cache
-def load_data():
-    url = "https://tracker.gg/valorant/profile/riot/reach%23lavi/overview"
-
+def load_webdriver(name, url, output = False):
     options = webdriver.ChromeOptions()
     options.add_argument('--ignore-certificate-errors')
     options.add_argument('--ignore-ssl-errors')
@@ -34,13 +31,33 @@ def load_data():
     driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
     driver.minimize_window()
     driver.get(url)
-
     wait = WebDriverWait(driver, 10)
     wait.until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, 'title')))
 
     soup = BeautifulSoup(driver.page_source, features="html.parser")
 
-    with open("output.txt", "w") as f:
-        print(soup.prettify(), file=f)
+    if output == True:
+        with open("{0}.html".format(name), "w", encoding="utf-8") as f:
+            f.write(str(soup.prettify()))
 
-load_data()
+    return soup
+
+# TODO: Run webdriver for top 100 players
+def load_player(soup):   
+    usernames = soup.find_all("td", class_="username")
+
+    for index, username in enumerate(usernames):
+        user_url = username.div.a['href']
+        print(str(index + 1) + " " + user_url)
+
+# TODO: View top 3 agents, vandal/phantom
+
+def main():
+    leaderboard_url = "https://tracker.gg/valorant/leaderboards/ranked/all/default?page=1&region=na"
+
+    soup = load_webdriver("leaderboard", leaderboard_url, output=True)
+
+    load_player(soup)
+
+if __name__ == "__main__":
+    main()
